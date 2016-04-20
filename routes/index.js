9f4155d6f1c966bfeb40a4b175a65d5bbfb7f163
Dipty Singhania <dsinghania@yahoo.com>
2016-04-20 10:25:01 -0500

    updated readme

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,3 @@ exports.index = function(req, res) {
     users: ['Ray', 'Morten', 'James']
   });
 }
-
-exports.about = function(req, res) {
-  res.render('default', {
-    title: 'About Us',
-    classname: 'about'
-  });
-}
