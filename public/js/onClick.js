13e09a4ca98b98a93056bf4b1b682c0732b86d8b
wombats17 <wombats17@gmail.com>
2016-04-23 01:33:48 -0500

    minor changes

--- public/js/onClick.js
+++ public/js/onClick.js
@@ -3,6 +3,7 @@ var db = new sqlite3.Database('../../datasets/nutrition.db');
 
 function Submitted() {
     var name = document.getElementById("name").value;
+	var portion = document.getElementById("portion").value;
     var sql = "SELECT * from NutritionData WHERE  Shrt_Desc like '" + name +"%'";  
     console.log(sql);
 }
