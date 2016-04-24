d4bf675bb65267e760dfda69111683cbcc72ebdc
Dipty Singhania <dsinghania@yahoo.com>
2016-04-24 13:23:45 -0500

    changes

--- public/js/onClick.js
+++ public/js/onClick.js
@@ -4,6 +4,6 @@ var db = new sqlite3.Database('../../datasets/nutrition.db');
 function Submitted() {
     var name = document.getElementById("name").value;
 	var portion = document.getElementById("portion").value;
-    var sql = "SELECT * from NutritionData WHERE  Shrt_Desc like '" + name +"%'";  
+    var sql = "SELECT * from NutritionData WHERE  Shrt_Desc like '" + name + "%'";  
     console.log(sql);
 }
