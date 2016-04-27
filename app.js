804c11bd3d2ddbb7e9d0f7d8158647e2e86da502
Dipty Singhania <dsinghania@yahoo.com>
2016-04-27 09:57:19 -0500

    updated

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 var express = require('express');
 var app = express();            //creates an express application
 var routes = require('./routes');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+var db = require('./db');
+
 //Lets define a port we want to listen to
 var PORT = process.env.port || 3000;
 
 var sqlite3 = require('sqlite3').verbose();  
 var db = new sqlite3.Database('./datasets/nutrition.db'); 
+passport.use(new Strategy(function (username, password, cb) { //cb-callback
+    db.users.findByUsername(username, function (err, user) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        if (user.password != password) { return cb(null, false); }
+        return cb(null, user);
+    });
+}));
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
 
 // Assigns / Sets view engine extension to ejs
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 // The app.locals object has properties that are local variables within the application.
@@ -18,6 +41,65 @@ app.get('/', routes.index);
 // validate all requests to the /api -based routes
 app.get('/search', routes.api);
 
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ 
+    secret: 'keyboard cat', 
+    resave: false, 
+    saveUninitialized: false }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ensure that the data on the session
+  // has been set for the first request
+  if(!req.session.viewCount){
+    req.session.viewCount = 0;
+  }
+
+  req.session.viewCount += 1;
+
+  // store arbitrary data that you need between
+  // requests, but is not important enough
+  // to put into a database
+  req.session.chosenIngredients = [
+    { id: 1, qty: 3}
+  ];
+
+  res.send("View Count: " + req.session.viewCount);
+// Define routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+  
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+  
+app.get('/logout',
+  function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
+app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('profile', { user: req.user });
+  });
 /*
 app.use("/api", require("./routes/api"));
 //app.use("/api", routes.api);
